d0405d9040465ff9902697454dfcc83eea85e269
Alexandre Gomes Gaigalas <alganet+github@gmail.com>
2025-10-30 02:17:13 -0300

    Add editor configuration files, C formatting & headers
    .compile_flags.txt (recognized by clangd extension)
    
     - Points to `build/descend` so `M2libc/bootstrappable.h` headers
       do not warn.
     - use `-std=c99`
    
    .clang-format
     - Helps keeping a C formatting standard
    
    .editorconfig
     - tabs/spaces standards
     - remove extra spaces from md files
    
    .gitattributes
     - helps code publishing tools to highlight kaem files as sh
    
    .vscode/extensions.json
     - Suggests EditorConfig extension
     - Suggests clangd extension
    
    .vscode/settings.json
     - helps editor to highlight kaem files as sh
    
    A k0.answers was regenerated because introducing the SPDX headers
    made the hashes change.
    
    The C files in the project were reformatted according to the new
    standard. Their implicit includes were also made explicit.

--- files/opt/abuild/src/zrpad.c
+++ files/opt/abuild/src/zrpad.c
@@ -1,64 +1,60 @@
+/* SPDX-FileCopyrightText: 2025 Alexandre Gomes Gaigalas <alganet@gmail.com> */
+/* SPDX-License-Identifier: GPL-3.0-or-later */
+
 #include <stdio.h>
 #include <stdlib.h>
 
 #include "M2libc/bootstrappable.h"
 
-int main(int argc, char** argv)
-{
-    int bs;
-    char* filename;
+int main(int argc, char **argv) {
+  int bs;
+  char *filename;
 
-    if (argc < 3)
-    {
-		fputs("zrpad requires 3 arguments\n", stderr);
-        exit(0);
-    }
-    
-    filename = argv[1];
-    bs = strtoint(argv[2]);
-    
-    FILE *fp = NULL;
-    FILE *fw = NULL;
-    long off;
+  if (argc < 3) {
+    fputs("zrpad requires 3 arguments\n", stderr);
+    exit(0);
+  }
 
-    fp = fopen(filename, "r");
-    if (fp == NULL)
-    {
-        fputs("failed to fopen\n", stderr);
-        exit(EXIT_FAILURE);
-    }
+  filename = argv[1];
+  bs = strtoint(argv[2]);
 
-    if (fseek(fp, 0, SEEK_END) == -1)
-    {
-        fputs("failed to fseek\n", stderr);
-        exit(EXIT_FAILURE);
-    }
+  FILE *fp = NULL;
+  FILE *fw = NULL;
+  long off;
 
-    off = ftell(fp);
-    
-    if (fclose(fp) != 0)
-    {
-        fputs("failed to fclose\n", stderr);
-        exit(EXIT_FAILURE);
-    }
+  fp = fopen(filename, "r");
+  if (fp == NULL) {
+    fputs("failed to fopen\n", stderr);
+    exit(EXIT_FAILURE);
+  }
 
-    if (off == -1)
-    {
-        fputs("failed to ftell\n", stderr);
-        exit(EXIT_FAILURE);
-    }
+  if (fseek(fp, 0, SEEK_END) == -1) {
+    fputs("failed to fseek\n", stderr);
+    exit(EXIT_FAILURE);
+  }
 
-    fw = fopen(argv[3], "w");
-    while (0 != off % bs) {
-        fputc('\0', fw);
-        off += 1;
-    }
+  off = ftell(fp);
 
-    if (fclose(fw) != 0)
-    {
-        fputs("failed to fclose\n", stderr);
-        exit(EXIT_FAILURE);
-    }
+  if (fclose(fp) != 0) {
+    fputs("failed to fclose\n", stderr);
+    exit(EXIT_FAILURE);
+  }
 
-    return 0;
+  if (off == -1) {
+    fputs("failed to ftell\n", stderr);
+    exit(EXIT_FAILURE);
+  }
+
+  fw = fopen(argv[3], "w");
+  while (0 != off % bs) {
+    fputc('\0', fw);
+    off += 1;
+  }
+
+  if (fclose(fw) != 0) {
+    fputs("failed to fclose\n", stderr);
+    exit(EXIT_FAILURE);
+  }
+
+  return 0;
 }
